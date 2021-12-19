a5e164171d92cff9aa3ac5e2180ee4e2c26cb926
Abhijeet Kumar <63132585+Abhijeet1520@users.noreply.github.com>
2021-12-19 16:56:44 +0530

    change knowyourcity to mapintel

--- knowyourcity/knowyourcity/asgi.py
+++ knowyourcity/knowyourcity/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for knowyourcity project.
+ASGI config for mapintel project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'knowyourcity.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mapintel.settings')
 
 application = get_asgi_application()
--- knowyourcity/knowyourcity/wsgi.py
+++ knowyourcity/knowyourcity/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for knowyourcity project.
+WSGI config for mapintel project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'knowyourcity.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mapintel.settings')
 
 application = get_wsgi_application()
